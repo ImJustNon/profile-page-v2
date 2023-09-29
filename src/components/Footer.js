ef44b159c4369302c77afa94dcd2434224a71253
ImJustNon <Im_Just_Non@hotmail.com>
2023-09-30 00:37:14 +0800

    Fix : Mobile footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 
 function Footer(){
     return(
-        <footer className="footer footer-center p-4 bg-ghost text-base z-10">
+        <footer className="footer footer-center p-4 bg-ghost text-base text-black z-10">
             <aside>
                 <p>Copyright © {new Date().getFullYear()} - <a href="https://me.nonlnwza.xyz" target="_blank" className="font-semibold">Nonlnwza.xyz</a></p>
             </aside>
