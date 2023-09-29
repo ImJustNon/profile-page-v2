b4e0c3d90c9378a8ac2e4c8828e67b13b0bbd0d2
ImJustNon <Im_Just_Non@hotmail.com>
2023-09-30 01:15:45 +0800

    Fix : Porject page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,13 +3,26 @@
 function Project(){
     return(
         <>
-            <div className="hero glass my-10 rounded-2xl text-start text-black">
+            <div className="hero glass my-10 rounded-2xl text-center text-black">
                 <div className="hero-content">
-                    <div className="py-5">
-                        <h1 className="text-5xl font-bold">ขี้เกียจ ทำโว้ยยยยยย</h1>
+                    <div className="py-2 px-2">
+                        <h1 className='text-2xl text-start mb-5'>{"<My Social Accounts />"}</h1>
+                        <hr className='my-5'/>
+                        {/* <div className="card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions">
+                                <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div> */}
                     </div>
                 </div>
-			</div>
+            </div>  
         </>
     );
 }
