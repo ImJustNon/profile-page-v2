79f017ce18eb7a4f1b10ff6e51127f7405f83799
ImJustNon <Im_Just_Non@hotmail.com>
2023-10-02 19:25:05 +0700

    Fix : Not load particle when  in bobile device

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ import Footer from './components/Footer';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Link, useLocation } from "react-router-dom";
 import Socials from './pages/Social';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 function App() {
 	const { pathname } = useLocation();
 
 	return (
-		<>
-			<Particle />
+		<>	
+			{isMobile ? <></> : <Particle />} {/* not load Particles js if in mobile device <it take resource too much :) > */}
+
 			<div className='mt-28 mx-auto w-[30rem] md:w-[45rem] noselect'>
 				<Profile />
 				<AnimatePresence wait>
