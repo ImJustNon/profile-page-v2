2de6a1491d92fd0ebee79d74f0e92d51f1574b0f
ImJustNon <Im_Just_Non@hotmail.com>
2024-09-15 02:17:04 +0700

    Udpate Fotter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ function Footer(){
     return(
         <footer className="footer footer-center p-4 bg-ghost text-base text-black z-10 my-5">
             <aside>
-                <p>Copyright © {new Date().getFullYear()} - <a href="https://me.nonlnwza.xyz" target="_blank" className="font-semibold">Nonlnwza.xyz</a></p>
+                <p>Copyright © 2022 - {new Date().getFullYear()}, All right reserved</p>
+                <p>Made with ❤️ by NO.eNd</p>
             </aside>
         </footer>
     );
